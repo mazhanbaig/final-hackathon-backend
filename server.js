4ea2aa8dbfd1a4bc39f20f762a78a69cc0ec30e3
mazhanbaig <mazhanbaig44@gmail.com>
2026-03-18 15:45:07 +0500

    index an dcors

--- server.js
+++ server.js
@@ -12,21 +12,21 @@
 //     console.log('Error while connecting DB')
 // })
 
-require('dotenv').config()
-const connectDB = require('./src/config/db')
-const app = require("./src/index")
+// require('dotenv').config()
+// const connectDB = require('./src/config/db')
+// const app = require("./src/index")
 
-module.exports = async (req, res) => {
-    return app(req, res);
-};
+// module.exports = async (req, res) => {
+//     return app(req, res);
+// };
 
-// For local development only
-if (process.env.NODE_ENV !== "production") {
-    connectDB().then(() => {
-        app.listen(5000, () => {
-            console.log("Server running at 5000");
-        });
-    });
-}
+// // For local development only
+// if (process.env.NODE_ENV !== "production") {
+//     connectDB().then(() => {
+//         app.listen(5000, () => {
+//             console.log("Server running at 5000");
+//         });
+//     });
+// }
 
 // eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY5YTk1ZDU3OWUwY2E3ZDYwMTE2NjUzNyIsImlhdCI6MTc3MjcwNzE1OSwiZXhwIjoxNzczMzExOTU5fQ.rVuTOTSmgkhmjBRyKRAk7IR1rhUAQqnffw3xrbUkxco
--- src/index.js
+++ src/index.js
@@ -1,19 +1,17 @@
 const express = require("express")
 const cors = require("cors")
-const authRoutes=require("./routes/auth.route")
+const authRoutes = require("./routes/auth.route")
+const connectDB=require("./config/db")
+
 const app = express()
 
-app.use(cors({
-    origin: "*",   
-    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-    credentials: true
-}));
+app.use(cors())
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
 app.use("/api/auth", authRoutes)
 
-connectDB().then(() => console.log("MongoDB connected")).catch(err => console.log(err));
+connectDB().then((res)=>console.log("db connected")).catch((error)=>console.log(error))
 
 module.exports = app
