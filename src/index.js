e25255e0dabf99427e28e466912e2f5be3a7bddc
mazhanbaig <mazhanbaig44@gmail.com>
2026-04-13 18:40:07 +0500

    final cors + vercel fix

--- src/index.js
+++ src/index.js
@@ -8,16 +8,12 @@ const app = express()
 app.use(cors({
     origin: "*",
     credentials: true,
-    allowedHeaders: ['Content-Type', 'Authorization']
+    allowedHeaders: ["Content-Type", "Authorization"]
 }))
-
-app.options("*", cors())
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
 app.use("/api/auth", authRoutes)
-
-connectDB().then((res)=>console.log("db connected")).catch((error)=>console.log(error))
 
 module.exports = app
